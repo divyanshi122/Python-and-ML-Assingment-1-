0bdf62d321d7c85591716f359ce987cf67f636bc
divyanshi122 <divyanshiagarwal2005@gmail.com>
2024-06-20 17:25:26 +0530

    Add files via upload

---
+++ 06.py
@@ -0,0 +1,4 @@
+# Write a program that reads the content of a text file and prints it to the console.
+samplefile = open('C:/Users/SHARAD/OneDrive/Desktop/New folder (2)/Demo.txt','r')
+
+print(samplefile)
